acf0e13c04aab145a04fef04a9d89a07afcea4ca
madhawap <madhawa.818@gmail.com>
2020-06-06 19:24:03 +1000

    Create dev branch and add dev code
    New dev branch is to keep gobbledgook seperate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import profilePicture from './img/myAvatar.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi! Madhawa here</h1>
+      <img src={profilePicture} alt="profilePicture" />
     </div>
   );
 }
