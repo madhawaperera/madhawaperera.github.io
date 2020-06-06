3670a012694dd4f00438ff39f2953c765195df7d
madhawap <madhawa.818@gmail.com>
2020-06-06 19:38:19 +1000

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Hi Madhawa here
---
+++ precache-manifest.3dd4c8457785a82c628ede295d0a6d24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd186fde652304ff2d2f230e1784da32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b2bfff8a2be7ebc29c6",
+    "url": "/static/css/main.6f3ab9c3.chunk.css"
+  },
+  {
+    "revision": "2906b822f10e5f90e913",
+    "url": "/static/js/2.8f010c7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f010c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2bfff8a2be7ebc29c6",
+    "url": "/static/js/main.48207487.chunk.js"
+  },
+  {
+    "revision": "657641ca2f3e4e4bb300",
+    "url": "/static/js/runtime-main.2834a61e.js"
+  },
+  {
+    "revision": "58e3cc6b95da86d33d9bae2c7faf8fcb",
+    "url": "/static/media/myAvatar.58e3cc6b.png"
+  }
+]);
