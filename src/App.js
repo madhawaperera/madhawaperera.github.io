73566f5f935cc1a350b0e800cfdbfc8f822363a1
madhawap <madhawa.818@gmail.com>
2020-06-07 00:04:16 +1000

    Modify avatar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import profilePicture from './img/myAvatar.png';
+import profilePicture from './img/myAvatar.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi! Madhawa here</h1>
-      <img src={profilePicture} alt="profilePicture" />
+        <img src={profilePicture} alt="profilePicture"  width="200" height="200" />
     </div>
   );
 }
